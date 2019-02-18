79d9e355e06b60342b6c452e78385dc41c4d24c5
einstein <37004313+Impeterlewis@users.noreply.github.com>
2019-02-18 10:16:59 -0500

    minorUpdate - Button fixes

--- app/src/main/java/com/night/einstein/SplashScreen.java
+++ app/src/main/java/com/night/einstein/SplashScreen.java
@@ -1,12 +1,25 @@
 package com.night.einstein;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.WindowManager;
 
+import su.levenetc.android.textsurface.Text;
+import su.levenetc.android.textsurface.TextBuilder;
+import su.levenetc.android.textsurface.TextSurface;
+import su.levenetc.android.textsurface.animations.Alpha;
+import su.levenetc.android.textsurface.animations.Delay;
+import su.levenetc.android.textsurface.animations.Sequential;
+import su.levenetc.android.textsurface.animations.Slide;
+import su.levenetc.android.textsurface.contants.Align;
+import su.levenetc.android.textsurface.contants.Side;
+
 public class SplashScreen extends AppCompatActivity {
+
+    TextSurface textAnimated;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +27,8 @@ public class SplashScreen extends AppCompatActivity {
         setContentView(R.layout.activity_splash_screen);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
 
 
         new Handler().postDelayed(new Runnable() {
@@ -31,5 +46,22 @@ public class SplashScreen extends AppCompatActivity {
             }
 
         }, 1000);
+
+     /*   textAnimated = findViewById(R.id.textAnimation);
+        Text text = TextBuilder
+                .create("einstein")
+                .setSize(30)
+                .setAlpha(0)
+                .setColor(Color.WHITE)
+                .setPosition(Align.SURFACE_CENTER).build();
+
+        textAnimated.play(
+                new Sequential(
+                        Slide.showFrom(Side.TOP, text, 500),
+                        Delay.duration(500),
+                        Alpha.hide(text, 1500)
+                )
+        ); */
+
     }
 }
--- app/src/main/java/com/night/einstein/courses/Philosophy/PhilosophyIntroContent.java
+++ app/src/main/java/com/night/einstein/courses/Philosophy/PhilosophyIntroContent.java
@@ -6,13 +6,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.night.einstein.R;
 import com.night.einstein.courses.Philosophy.contents.PhilosophyIntroLogic;
 
 public class PhilosophyIntroContent extends AppCompatActivity {
 
-    Button logicLayout, close;
+    Button logicLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,17 +22,6 @@ public class PhilosophyIntroContent extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        close = findViewById(R.id.close);
-        close.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //Moves from the current activity to the MainActivity
-                Intent terminate = new Intent(PhilosophyIntroContent.this, PhilosophyIntroActivity.class);
-                terminate.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(terminate);
-                finish();
-            }
-        });
 
         logicLayout = findViewById(R.id.logicButton);
         logicLayout.setOnClickListener(new View.OnClickListener() {
@@ -43,5 +33,14 @@ public class PhilosophyIntroContent extends AppCompatActivity {
             }
         });
 
+        Button close = findViewById(R.id.close);
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PhilosophyIntroContent.this, PhilosophyIntroActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
     }
 }
