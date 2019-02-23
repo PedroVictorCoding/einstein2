7691e511156810e5767ac3ed7735e15bc71d755a
einstein <37004313+Impeterlewis@users.noreply.github.com>
2019-02-23 16:00:24 -0500

    not Major

--- app/src/main/java/com/night/einstein/courses/Philosophy/contents/PhilosophyIntroLogic.java
+++ app/src/main/java/com/night/einstein/courses/Philosophy/contents/PhilosophyIntroLogic.java
@@ -2,18 +2,20 @@ package com.night.einstein.courses.Philosophy.contents;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.WindowManager;
+import android.util.DisplayMetrics;
+import android.view.Display;
 
 import com.night.einstein.R;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class PhilosophyIntroLogic extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_philosopy_intro_logic);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_physics_intro);
 
     }
 }
