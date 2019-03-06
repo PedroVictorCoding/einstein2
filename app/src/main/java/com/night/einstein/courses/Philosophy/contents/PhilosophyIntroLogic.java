43b310945680a954265e955e2ed956c9aba32728
Pedro <pedro.luis.almeida.victor@gmail.com>
2019-03-06 17:49:21 -0500

    Logic Learning UI

--- app/src/main/java/com/night/einstein/courses/Philosophy/contents/PhilosophyIntroLogic.java
+++ app/src/main/java/com/night/einstein/courses/Philosophy/contents/PhilosophyIntroLogic.java
@@ -12,10 +12,27 @@ import java.util.List;
 
 public class PhilosophyIntroLogic extends AppCompatActivity {
 
+Button close;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_physics_intro);
+        setContentView(R.layout.activity_physics_intro_logic);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        close = findViewById(R.id.goBack);
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PhysicsIntroLogic.this, PhilosophyIntroActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                overridePendingTransition(0, 0);
+                startActivity(intent);
+            }
+        });
+
+
 
     }
 }
